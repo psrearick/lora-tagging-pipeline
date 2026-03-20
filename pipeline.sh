79ba2e083eeb569653dbf9328668b7e95b000916
Phillip Rearick <psrearick@gmail.com>
2026-03-20 10:49:48 -0400

    wip

---
+++ pipeline.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+CUR="$(pwd)"
+
+dirx="$(dirname -- $(readlink -fn -- "$0"; echo x))";
+DIR="${dirx%x}";
+cd "$DIR"
+
+source "$DIR/sources.conf"
+
+CROP="$DEST/cropped"
+LABELS="$DEST/clip_labels.json"
+
+function download() {
+    "$CUR/download.sh"
+}
+
+function filter() {
+    "$CUR/filter.sh"
+}
+
+function crop() {
+    poetry run python "$CUR/smart_crop.py" "$DEST/sorted/accepted" "$CROP"
+}
+
+function dedupe() {
+    poetry run python "$CUR/dedupe.py" "$CROP"
+}
+
+function clip() {
+    poetry run python "$CUR/clip_label.py" "$CROP" "$LABELS"
+}
+
+function generate() {
+    poetry run python "$CUR/generate_data.py" "$LABELS" "$DEST/data.json"
+}
+
+function open_gallery() {
+    open http://localhost:8000/gallery.html
+}
+
+function review() {
+    echo ""
+}
+
+function export_training() {
+    echo ""
+}
+
+function update_gallery() {
+    cp gallery.html "$DEST/gallery.html"
+}
+
+function all() {
+    download
+    filter
+    crop
+    dedupe
+    clip
+    generate
+    review
+    export_training
+}
+
+function usage() {
+    echo "Usage: ./pipeline.sh [download|filter|crop|dedupe|clip|generate|review|export_training|all]"
+    exit 1
+}
+
+case "$1" in
+    download)
+        # Shift the arguments so $1 within the function refers to the name, not the command
+        shift
+        download
+        ;;
+    filter)
+        shift
+        filter
+        ;;
+    crop)
+        shift
+        crop
+        ;;
+    dedupe)
+        shift
+        dedupe
+        ;;
+    clip)
+        shift
+        clip
+        ;;
+    generate)
+        shift
+        generate
+        ;;
+    review)
+        shift
+        review
+        ;;
+    export)
+        shift
+        export_training
+        ;;
+    all)
+        shift
+        all
+        ;;
+    update-gallery)
+        shift
+        update_gallery
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+cd "$CUR"
